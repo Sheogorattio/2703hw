1aeee5237b653af92979bb6734d7896568235178
Sheogorattio <91422100+Sheogorattio@users.noreply.github.com>
2024-05-10 04:32:17 +0300

    Add project files.

---
+++ 2703hw/Controllers/BookController.cs
@@ -0,0 +1,45 @@
+﻿using _2703hw.Models;
+using _2703hw.Services;
+using _2703hw.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+using System.Text;
+
+namespace _2703hw.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly BookService _bookService;
+        public BookController(BookService bookService)
+        {
+            _bookService = bookService;
+        }
+        public IActionResult Index()
+        {
+            var books = _bookService.GetAllBooks();
+            return View(books);
+        }
+        public IActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Add(BookViewModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                var book = new Book
+                {
+                    Title = model.Title,
+                    Author = model.Author,
+                    Genre = model.Genre,
+                    Year = model.Year
+                };
+                _bookService.AddBook(book);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(model);
+        }
+
+    }
+}
---
+++ 2703hw/Services/BookService.cs
@@ -0,0 +1,28 @@
+﻿using _2703hw.Models;
+
+namespace _2703hw.Services
+{
+    public class BookService
+    {
+        private List<Book> _books;
+        public BookService()
+        {
+            _books = new List<Book>()
+            {
+                new Book() {Id=1, Title="Lord of the Flies", Author=" William Golding", Genre="Dystopian", Year=1954},
+                new Book() {Id=2, Title="Blood Meridian", Author=" Cormac McCarthy", Genre="Western", Year=1985},
+                new Book() {Id=3, Title="For Whom the Bell Tols", Author="Ernest Hemingway", Genre="War novel", Year=1940},
+
+            };
+        }
+        public IEnumerable<Book> GetAllBooks()
+        {
+            return _books;
+        }
+        public void AddBook(Book book)
+        {
+            _books.Add(book);
+        }
+
+    }
+}
